eb82b58788fdd75796bbddaa64d9d075cee6de4e
Rtanay28 <48868671+Rtanay28@users.noreply.github.com>
2020-03-27 15:42:16 +0600

    Update Script.m

--- Script.m
+++ Script.m
@@ -1,13 +1,11 @@
-clc
-clear
+clc;close all;clear all;
 
-a=importdata('1.dat');
-x=a(:,1);
-y=a(:,2);
-p1=polyfit(x,y,5);
-y1=polyval(p1,x);
-
-%p5=polyfit(x,y,5);
-%y5=polyval(p5,x);
-
-plot(x,y,x,y1,'r');
+x=linspace(0,0.09,100); %flow domain discretized in 100 points
+%All air property values in SI
+Tinf=293; %temperature of incoming air
+Rho=1.205; % density of air at 293K
+k=0.0257; %thermal conductivity
+Cp=1005; %specific heat
+Mu=1.82*10^-5; %Dynamic Viscosity
+Nu=15.11*10^-6; %Kinematic Viscosity
+Dh=0.016; 
